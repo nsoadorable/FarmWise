1905b9f1bcbd23d3f83c39d9a50b69ea71c8435b
YsaRivera <ysabelladanielle.rivera.cics@ust.edu.ph>
2025-05-28 16:13:44 +0800

    clickable logo and title

--- farmwise/src/components/Navbar.js
+++ farmwise/src/components/Navbar.js
@@ -32,40 +32,100 @@ export default function Navbar() {
   return (
     <AppBar position="static" sx={{ backgroundColor: '#4b644a' }}>
       <Toolbar>
-        <Box component="img" src="img/FW_Logo.png" alt="FarmWise Logo" sx={{ height: 50, mr: 2 }} />
-        <Typography variant="h6" sx={{ flexGrow: 1, fontFamily: 'Sitka Text, serif' }}>
-          FarmWise
-        </Typography>
-        {mainLinks.map(link => (
-          <Button
-            key={link.path}
-            component={Link}
-            to={link.path}
-            sx={{ color: '#fff', fontFamily: 'Merriweather, serif', '&:hover': { backgroundColor: '#84c461', color: '#341c1c' } }}
+        {/* Logo and Title Group (both clickable) */}
+        <Box 
+          component={Link} 
+          to="/" 
+          sx={{ 
+            display: 'flex', 
+            alignItems: 'center', 
+            textDecoration: 'none',
+            flexGrow: 1,
+            '&:hover': {
+              opacity: 0.9
+            }
+          }}
+        >
+          <Box 
+            component="img" 
+            src="img/FW_Logo.png" 
+            alt="FarmWise Logo" 
+            sx={{ 
+              height: 50, 
+              mr: 2,
+              cursor: 'pointer'
+            }} 
+          />
+          <Typography 
+            variant="h6" 
+            sx={{ 
+              fontFamily: 'Sitka Semibold, serif',
+              color: '#fff',
+              cursor: 'pointer',
+              '&:hover': {
+                color: '#84c461'
+              }
+            }}
           >
-            {link.label}
-          </Button>
-        ))}
-        {!isLoggedIn
-          ? guestLinks.map(link => (
-              <Button
-                key={link.path}
-                component={Link}
-                to={link.path}
-                sx={{ color: '#fff', fontFamily: 'Merriweather, serif', '&:hover': { backgroundColor: '#84c461', color: '#341c1c' } }}
-              >
-                {link.label}
-              </Button>
-            ))
-          : authLinks.map(link => (
-              <Button
-                key={link.label}
-                onClick={link.action}
-                sx={{ color: '#fff', fontFamily: 'Merriweather, serif', '&:hover': { backgroundColor: '#84c461', color: '#341c1c' } }}
-              >
-                {link.label}
-              </Button>
-            ))}
+            FarmWise
+          </Typography>
+        </Box>
+
+        {/* Navigation Links */}
+        <Box sx={{ display: 'flex' }}>
+          {mainLinks.map(link => (
+            <Button
+              key={link.path}
+              component={Link}
+              to={link.path}
+              sx={{ 
+                color: '#fff', 
+                fontFamily: 'Merriweather, serif', 
+                '&:hover': { 
+                  backgroundColor: '#84c461', 
+                  color: '#341c1c' 
+                } 
+              }}
+            >
+              {link.label}
+            </Button>
+          ))}
+          
+          {!isLoggedIn
+            ? guestLinks.map(link => (
+                <Button
+                  key={link.path}
+                  component={Link}
+                  to={link.path}
+                  sx={{ 
+                    color: '#fff', 
+                    fontFamily: 'Merriweather, serif', 
+                    '&:hover': { 
+                      backgroundColor: '#84c461', 
+                      color: '#341c1c' 
+                    } 
+                  }}
+                >
+                  {link.label}
+                </Button>
+              ))
+            : authLinks.map(link => (
+                <Button
+                  key={link.label}
+                  onClick={link.action}
+                  sx={{ 
+                    color: '#fff', 
+                    fontFamily: 'Merriweather, serif', 
+                    '&:hover': { 
+                      backgroundColor: '#84c461', 
+                      color: '#341c1c' 
+                    } 
+                  }}
+                >
+                  {link.label}
+                </Button>
+              ))}
+        </Box>
       </Toolbar>
     </AppBar>
   );
